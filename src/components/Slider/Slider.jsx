6b5146fa4b07182aa2031efa8a3490c9869f3c95
Xrene3 <ryanclarkgeneveo03@gmail.com>
2026-03-29 17:46:07 +0800

    Changed layout to use vetical scrolling instead of carousel / pagination style

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -12,45 +12,52 @@ export default function Slider({ children, page, setPage, isMobile }) {
 
     return (
         <>
-            {isMobile ?
-                <div className="flex flex-col gap-5 justify-center w-full">
-                    <div className="p-2 5 z-50 flex flex-col gap-y-5 mb-5">
-                        {children}
-                    </div>
+            <div className="flex flex-col gap-5 justify-center">
+                <div className="p-2.5 z-50 flex flex-col justify-center w-full gap-y-15 mb-5">
+                    {children}
                 </div>
+            </div>
 
-                :
-                <div className="flex justify-between h-full">
-                    <div className={`${page < 1 ? 'hidden' : ''} fixed absolute md:top-1/2 left-0 z-70 md:me-5`}>
-                        <button
-                            onClick={prevSlide}
-                            className="py-5 rounded 
-                            bg-sky-50/40 dark:bg-neutral-400/20
-                            text-zinc-600 dark:text-indigo-100 hover:text-orange-500/80 ease-linear duration-150
-                            border border-gray-400/60 shadow-lg px-4">
-                            <FaArrowLeft />
-                        </button>
-                    </div>
+            {
+            // isMobile ?
+            //     <div className="flex flex-col gap-5 justify-center w-full">
+            //         <div className="p-2 5 z-50 flex flex-col gap-y-5 mb-5">
+            //             {children}
+            //         </div>
+            //     </div>
 
-                    {/* Main Content Area */}
-                    <div className="flex justify-center mt-[6vh] w-full z-50 md:px-30">
-                        <div className="2xl:w-5/7  lg:w-[80vw] md:w-4/5 px-4 z-10 mt-1/2 flex flex-col gap-y-8">
-                            {Array.isArray(children) ? children[page] : children}
-                            {/* {children} */}
-                        </div>
-                    </div>
+            //     :
+            //     <div className="flex justify-between h-full">
+            //         <div className={`${page < 1 ? 'hidden' : ''} fixed absolute md:top-1/2 left-0 z-70 md:me-5`}>
+            //             <button
+            //                 onClick={prevSlide}
+            //                 className="py-5 rounded 
+            //                 bg-sky-50/40 dark:bg-neutral-400/20
+            //                 text-zinc-600 dark:text-indigo-100 hover:text-orange-500/80 ease-linear duration-150
+            //                 border border-gray-400/60 shadow-lg px-4">
+            //                 <FaArrowLeft />
+            //             </button>
+            //         </div>
 
-                    <div className={`${page >= totalPages - 1 ? 'hidden' : ''} fixed absolute md:top-1/2 right-0 z-70 md:ms-5`}>
-                        <button
-                            onClick={nextSlide}
-                            className="py-5 rounded 
-                            bg-sky-50/40 dark:bg-neutral-400/20
-                            text-zinc-600 dark:text-indigo-100 hover:text-orange-500/80 ease-linear duration-150
-                            border border-gray-400/60 shadow-lg px-4 md:ms-5">
-                            <FaArrowRight />
-                        </button>
-                    </div>
-                </div>
+            //         {/* Main Content Area */}
+            //         <div className="flex justify-center mt-[6vh] w-full z-50 md:px-30">
+            //             <div className="2xl:w-5/7  lg:w-[80vw] md:w-4/5 px-4 z-10 mt-1/2 flex flex-col gap-y-8">
+            //                 {Array.isArray(children) ? children[page] : children}
+            //                 {/* {children} */}
+            //             </div>
+            //         </div>
+
+            //         <div className={`${page >= totalPages - 1 ? 'hidden' : ''} fixed absolute md:top-1/2 right-0 z-70 md:ms-5`}>
+            //             <button
+            //                 onClick={nextSlide}
+            //                 className="py-5 rounded 
+            //                 bg-sky-50/40 dark:bg-neutral-400/20
+            //                 text-zinc-600 dark:text-indigo-100 hover:text-orange-500/80 ease-linear duration-150
+            //                 border border-gray-400/60 shadow-lg px-4 md:ms-5">
+            //                 <FaArrowRight />
+            //             </button>
+            //         </div>
+            //     </div>
             }
         </>
     )
