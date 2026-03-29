c4e7e14db5843e2f3f5697d018a250ea2cf91a4e
Xrene3 <ryanclarkgeneveo03@gmail.com>
2026-03-29 17:45:40 +0800

    Modified layout and added reference for v2 portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -39,34 +39,45 @@ function App() {
     <>
       <main className="
       relative 
-      w-full min-h-screen
-      bg-gradient-to-b dark:from-zinc-900 dark:to-indigo-900 
+      w-full 
+      min-h-screen
+      bg-gradient-to-b dark:from-zinc-900 dark:to-indigo-700 
       from-blue-300 to-white
       dark:text-sky-50 text-zinc-900
       ease-linear duration-300
       ">
-        <div className="top-0 m-0 dark-mode-switch w-auto relative">
-          <div className="flex justify-center gap-3">
-            <button
-              className=""
-              onClick={toggleTheme}>
-              {theme == 'dark' ? <FaSun>Toggle Light Mode</FaSun> : <FaMoon>Toggle Darkmode </FaMoon>}
-            </button>
+        <div className="container w-full max-w-6xl mx-auto">
+          <div className="top-0 m-0 dark-mode-switch w-auto relative">
+            <div className="flex justify-center gap-3">
+              <button
+                className=""
+                onClick={toggleTheme}>
+                {theme == 'dark' ? <FaSun>Toggle Light Mode</FaSun> : <FaMoon>Toggle Darkmode </FaMoon>}
+              </button>
 
-            <button className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded" onClick={() => setWave(prev => !prev)}>
-              {isWaving ? <FaPlay></FaPlay> : <FaPause>Pause wave</FaPause>}
-            </button>
+              <button className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded" onClick={() => setWave(prev => !prev)}>
+                {isWaving ? <FaPlay></FaPlay> : <FaPause>Pause wave</FaPause>}
+              </button>
+            </div>
+            
+            <div className="w-full text-center my-2.5 p-3 text-lg rounded-lg 
+              bg-purple-200/40 dark:bg-lime-800/40
+              border-1 border-purple-600/60 dark:border-lime-200/60
+              ">
+                <p>
+                  I'm currently working on a <a href="https://xrene-resume-v2.vercel.app/" target="_blank" className='font-bold text-violet-600 dark:text-lime-200 underline'>v2 for my portfolio</a> using NextJS, React, Tailwind and Daisy UI
+                </p>
+              </div>
           </div>
+
+          <Slider page={page} setPage={setPage} isMobile={isMobile}>
+            <Welcome />
+            <AboutMe />
+            <Skills />
+            <Projects setIsHoveringCard={setIsHoveringCard} play={play} stop={stop} isOpen={isProjectsOpen} setIsOpen={setProjectsOpen} />
+            <Contacts setIsHoveringCard={setIsHoveringCard} play={play} stop={stop} isOpen={isContactsOpen} setIsOpen={setContactsOpen} />
+          </Slider>
         </div>
-
-        <Slider page={page} setPage={setPage} isMobile={isMobile}>
-          <Welcome />
-          <AboutMe />
-          <Skills />
-          <Projects setIsHoveringCard={setIsHoveringCard} play={play} stop={stop} isOpen={isProjectsOpen} setIsOpen={setProjectsOpen} />
-          <Contacts setIsHoveringCard={setIsHoveringCard} play={play} stop={stop} isOpen={isContactsOpen} setIsOpen={setContactsOpen} />
-        </Slider>
-
         {/* Water footer */}
         <Waves isWaving={isWaving} isMobile={isMobile} theme={theme} />
       </main >
